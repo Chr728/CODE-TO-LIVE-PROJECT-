3b70e8c44203a7f7956cbfe44db35681d34b8ba9
raynard <raynardomongbale@gmail.com>
2021-09-16 00:19:15 -0400

    server up on port 8080

---
+++ backend/go.mod
@@ -0,0 +1,9 @@
+module FitMood/backend
+
+go 1.16
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20210915214749-c084706c2272 // indirect
+)
--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,15 @@
 package main
+
+import "github.com/labstack/echo"
+
+func main() {
+	// new Echo instance
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.JSONPretty(200, "Homepage", "")
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+
+}
